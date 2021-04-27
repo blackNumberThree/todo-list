7628c7f59cb688200ac1260d500b3bfa85254efa
NikonorIvanovich <bid93@mail.ru>
2021-04-27 13:15:05 +0300

    fixed css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom';
-import{App} from './component/app'
+import{App} from './component/app';
+import './style.css'
 
 ReactDOM.render(<App/>,document.getElementById('root'))
 
